377e724600657edbb257b2fea0389193215b9bb2
Asen Asenov <aasenov1989@gmail.com>
2015-03-25 22:50:57 +0200

    Add RestAPI service. Update documentation.

---
+++ RestAPI/src/Test.java
@@ -0,0 +1,19 @@
+import org.restlet.Server;
+import org.restlet.data.Protocol;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+
+public class Test extends ServerResource {
+
+    public static void main(String[] args) throws Exception {
+        // Create the HTTP server and listen on port 8182
+        new Server(Protocol.HTTP, 8182, Test.class).start();
+    }
+
+    @Get("txt")
+    public String toString() {
+        return "hello, world";
+    }
+
+}
