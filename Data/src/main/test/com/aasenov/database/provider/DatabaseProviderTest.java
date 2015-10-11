5e4b82d232ebc72cc3eff44da6512474678ed767
Asen Asenov <aasenov1989@gmail.com>
2015-10-11 14:59:02 +0300

    Make structure for Unit testing
    Change build structure to not include test classes
    Make some dummy test cases to test the build
    Pack final jar from all projects jars, not from their bin dirs

---
+++ Data/src/main/test/com/aasenov/database/provider/DatabaseProviderTest.java
@@ -0,0 +1,21 @@
+package com.aasenov.database.provider;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+/**
+ * Testing {@link DatabaseProvider} functionalities.
+ */
+public class DatabaseProviderTest extends TestCase {
+
+    @Test
+    public void testSetDatabaseType() {
+
+    }
+
+    @Test
+    public void testGetDefaultManager() {
+
+    }
+}
